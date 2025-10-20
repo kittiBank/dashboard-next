afe06eb0a6877201c11d293ceca744f4f8aa4851
kittiBank <kittitach.pro@gmail.com>
2025-10-20 20:35:30 +0700

    feat: add metadata in all pages

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,10 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Customers",
+  description:
+    "The official Next.js Learn Course Dashboard, built with App Router.",
+};
 export default function Page() {
   return <p>Customer Page</p>;
 }
